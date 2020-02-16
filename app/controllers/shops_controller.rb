ae67e93e2dfaa79133045b67912077e467a4e593
Phtran0151 <phuong.tran@tomosia.com>
2020-02-16 21:15:55 +0700

    add page home shops

---
+++ app/controllers/shops_controller.rb
@@ -0,0 +1,10 @@
+class ShopsController < ApplicationController
+  def index
+  	@products = Product.all
+  	@cart_item = current_cart.cart_items.new
+  end
+
+  def show
+  	@product = Product.find(params[:id])
+  end
+end
