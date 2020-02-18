28a7ae4dae1a08ae0cf714b8ff9ccd39487a033d
Phtran0151 <phuong.tran@tomosia.com>
2020-02-18 14:03:03 +0700

    a

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,22 +1,15 @@
 class OrderItemsController < ApplicationController
 
   def create
-    chosen_product = Product.find(params[:product_id])
-    current_order = @current_order
+      current_order = @current_order
+      binding.pry
+      @order_item = OrderItem.new(order_item_params)
+      if @order_item.save
+        redirect_to orders_path(current_order), notice: "Thank you."
+      else
+        redirect_to orders_path(current_order), alert: 'Unable to add comment'
+      end
 
-    # If cart already has this product then find the relevant
-    binding.pry
-    if current_order.user.include?(chosen_product)
-      @order_item = current_order.order_items.find_by(:product_id => chosen_product)
-      @order_item.quantity += 1
-    else
-      @order_item = OrderItem.new
-      @order_item.order = current_order
-      @order_item.product = chosen_product
-    end
-
-    @order_item.save
-    redirect_to orders_path(current_order)
   end
 
   # DELETE /order_items/1
@@ -29,6 +22,6 @@ class OrderItemsController < ApplicationController
 
   private
     def order_item_params
-      params.require(:order_item).permit(:quantity, :product_id, :cart_id)
+      params.require(:order_items).permit(:product_id, :cart_id, :quantity)
     end
 end
